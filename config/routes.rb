a51871a4b450533bd6661c73ce8bde57fd16460c
naveen <naveen>
2016-08-30 15:21:05 +0200

    flyers controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,15 @@ Rails.application.routes.draw do
   resources :broadcasts
 
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
+        sessions: 'users/sessions',
+            registrations: 'flyers/registrations'
+
       }
+
+  # devise_for :flyers, controllers: {
+  #   sessions: 'flyers/sessions',
+  # }
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
